a9234e55c6411293a8b192fca8a191e404376c0a
Pawan <Pawan@192.168.0.104>
2022-06-13 11:36:51 +0530

    pushing the code first time

---
+++ CONSTRUCTOR/src/pkg/classABC.java
@@ -0,0 +1,24 @@
+package pkg;
+
+public class classABC
+{
+	 public classABC() 
+	 {
+		System.out.println("default constructor");// TODO Auto-generated constructor stub
+	}
+	 public classABC(int a , int b) 
+	 {
+		System.out.println("two parameterized constructor");// TODO Auto-generated constructor stub
+	}
+	 public classABC(int a) 
+	 {
+		System.out.println("one parameterized constructor");// TODO Auto-generated constructor stub
+	}
+	 public static void main(String[] args)
+	 {
+		classABC obj=new classABC(34,12);
+		classABC obj2=new classABC();
+		classABC obj3=new classABC(22);
+		
+	}
+}
